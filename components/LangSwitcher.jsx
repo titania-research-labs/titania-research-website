5665b4ed5f6f481a2d3e3b14080c5b5abc232a6f
banri <banri.yanahama@gmail.com>
2024-09-13 19:47:09 +0900

    fix langwage switcher #39

--- components/LangSwitcher.jsx
+++ components/LangSwitcher.jsx
@@ -7,15 +7,17 @@ const LangSwitcher = () => {
   return (
     <div className='lang-switcher flex space-x-2 text-[10px] text-gray-500 ml-6 items-center'>
       <button
-        className={`lang-switcher-button ${isJapanese && 'active'}`}
-        onClick={toggleLanguage}
+        className={'lang-switcher-button'}
+        onClick={() => isJapanese && toggleLanguage()}
+        disabled={!isJapanese}
       >
         EN
       </button>
       <div>/</div>
       <button
-        className={`lang-switcher-button ${!isJapanese && 'active'}`}
-        onClick={toggleLanguage}
+        className={'lang-switcher-button'}
+        onClick={() => !isJapanese && toggleLanguage()}
+        disabled={isJapanese}
       >
         JA
       </button>
